e7dd0220c092261c6cac1aeb35e873b4755a6ff5
SGNayak12 <GNayakSuhas12@gmail.com>
2024-07-15 22:51:24 +0530

    Done with custome api and error handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,17 @@
 import express from 'express';
-const app=express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
+const app=express();
+app.use(express.json({limit:"32kb"}));
+app.use(express.urlencoded({extended:true,limit:"32kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
 
 
 export {app};
