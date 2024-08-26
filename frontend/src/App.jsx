65477b451ea0e18b579680802f734dfb835230d7
SGNayak12 <GNayakSuhas12@gmail.com>
2024-08-26 18:17:31 +0530

    added tailwind css configuration for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,9 @@
 import { useState,useEffect} from 'react'
-import './App.css'
-
-
+import Navbar from './components/Navbar'
 function App() {
-  
-  
-
-
   return (
     <>
-    <h1>React App</h1>
+    <Navbar/>
     </>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <h1 className='text-center font-mono font-extrabold text-4xl'>Hello world</h1>
+    </>
+  )
+}
+
+export default Navbar
